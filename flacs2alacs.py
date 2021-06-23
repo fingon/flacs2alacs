3cfc89c80dbd557754d9aaaa456a155b10d91f91
Markus Stenberg <markus.stenberg@iki.fi>
2021-06-23 21:39:12 +0300

    Missing jpeg file not fatal, use local tempdir instead of tempfilename

--- flacs2alacs.py
+++ flacs2alacs.py
@@ -7,13 +7,14 @@
 # Copyright (c) 2018 Markus Stenberg
 #
 # Created:       Fri Nov  9 10:07:49 2018 mstenber
-# Last modified: Fri Nov  9 11:36:38 2018 mstenber
-# Edit time:     35 min
+# Last modified: Wed Jun 23 21:35:29 2021 mstenber
+# Edit time:     38 min
 #
 
 import os
 import os.path
 import subprocess
+import tempfile
 
 
 def get_flacs_and_covers(path):
@@ -27,7 +28,9 @@ def get_flacs_and_covers(path):
         jpgs = [x for x in filenames_and_suffixes
                 if x[1] in ['jpg', 'jpeg'] and '-resized-' not in x[0]]
         # atomicparsley seems to stick -resized- files all over the place, sigh
-        assert jpgs, 'no .jpg in %s' % dirpath
+        if not jpgs:
+            print('!!! no .jpg in %s' % dirpath)
+            continue
         for flac in flacs:
             assert dirpath[:len(path)] == path
             relpath = dirpath[len(path)+1:]
@@ -40,35 +43,36 @@ def convert_flac_to_alac(srcdir, dstdir, t):
     srcpath = os.path.join(srcdir, dirname, flacname)
     jpgpath = os.path.join(srcdir, dirname, jpegname)
     dstpath = os.path.join(dstdir, dirname, alacname)
-    tmppath = os.path.join(dstdir, dirname, 'tmp.' + alacname)
-    st0 = os.stat(srcpath)
-    try:
-        st = os.stat(dstpath)
-        uptodate = st0.st_mtime <= st.st_mtime
-    except FileNotFoundError:
-        uptodate = False
-    if uptodate:
-        return
-    os.makedirs(os.path.dirname(dstpath), exist_ok=True)
-    try:
-        os.unlink(tmppath)
-    except FileNotFoundError:
-        pass
-    subprocess.run(['ffmpeg', '-i', srcpath,
-                    '-vn',  # do not make video even if it has art :p
-                    '-acodec', 'alac',
-                    tmppath], check=True)
-    subprocess.run(['AtomicParsley', tmppath,
-                    '--artwork', jpgpath,
-                    '--overWrite'], check=True)
-    # print(tmppath, dstpath)
-    os.rename(tmppath, dstpath)
+    with tempfile.TemporaryDirectory(dir=dstdir) as tempdir:
+        tmppath = os.path.join(tempdir, alacname)
+        st0 = os.stat(srcpath)
+        try:
+            st = os.stat(dstpath)
+            uptodate = st0.st_mtime <= st.st_mtime
+        except FileNotFoundError:
+            uptodate = False
+        if uptodate:
+            return
+        os.makedirs(os.path.dirname(dstpath), exist_ok=True)
+        try:
+            os.unlink(tmppath)
+        except FileNotFoundError:
+            pass
+        subprocess.run(['ffmpeg', '-i', srcpath,
+                        '-vn',  # do not make video even if it has art :p
+                        '-acodec', 'alac',
+                        tmppath], check=True)
+        subprocess.run(['AtomicParsley', tmppath,
+                        '--artwork', jpgpath,
+                        '--overWrite'], check=True)
+        # print(tmppath, dstpath)
+        os.rename(tmppath, dstpath)
 
 
 if __name__ == '__main__':
-    import sys
-    import multiprocessing
     import functools
+    import multiprocessing
+    import sys
     srcdir, dstdir = sys.argv[1:]  # two mandatory arguments
     l = get_flacs_and_covers(srcdir)
     l = list(l)
